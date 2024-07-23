c473a85267774ef0428645a40288a8f007fcd3b8
ThejaswiG <63565875+ThejaswiG@users.noreply.github.com>
2024-07-23 15:22:00 -0500

    added controller

---
+++ src/main/java/com/example/ecommerce/ordercontroller.java
@@ -0,0 +1,37 @@
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.model.Order;
+import com.example.ecommerce.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteOrder(@PathVariable Long id) {
+        return orderService.deleteOrder(id);
+    }
+}
